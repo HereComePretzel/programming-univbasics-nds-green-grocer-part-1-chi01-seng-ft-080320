6f1606a38fda6040f43aaabe1fa6c3e537103fcf
Austin Oie <austinoie@gmail.com>
2020-07-14 17:49:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,7 +9,12 @@
 def find_item_by_name_in_collection(name, collection)
   counter = 0
   while counter < collection.length 
-  if 
+    if collection[counter][:item] == name
+      return collection[counter]
+    end
+    counter += 1 
+  end 
+end 
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
