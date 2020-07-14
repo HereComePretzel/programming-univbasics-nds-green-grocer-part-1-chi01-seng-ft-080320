fff5597712ba7b673820cad277d6621049393ea2
Austin Oie <austinoie@gmail.com>
2020-07-14 17:44:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,14 +9,7 @@
 def find_item_by_name_in_collection(name, collection)
     counter = 0
     while collection < collection.length 
-    collection.each do |name, collection|
-      if collection[counter][:item] == name   
-        return collection[counter][:item]   
-      else
-      counter += 1
-      end 
-    end 
-    end 
+     
   
 
   # Implement me first!
