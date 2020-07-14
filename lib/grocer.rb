cee9dd4055103de5c5900ce02e02b36d737f33bc
Austin Oie <austinoie@gmail.com>
2020-07-14 17:26:55 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -8,7 +8,7 @@
 
 def find_item_by_name_in_collection(name, collection)
     collection.each do |name, collection|
-      #binding.pry 
+      binding.pry 
       if collection == name 
         return name  
       else
